19f423aeca72009cf153f68a750cfb3640d41542
sotacode <sotacodeprivate@gmail.com>
2023-12-21 16:30:56 +1300

    first elements added

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { decrement, increment } from './store/slices/counter/counterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const {counter} = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <div>{counter}</div>
+        <button onClick={() => dispatch(increment())}>
+          increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          decrement
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
